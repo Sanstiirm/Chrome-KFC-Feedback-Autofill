1d1c39cee3decefdc9ffabf41751222cfa31727a
flynsarmy <flynsarmy@gmail.com>
2017-04-24 14:15:34 +1000

    first commit

---
+++ inject.js
@@ -0,0 +1,46 @@
+(function () {
+    // First page
+    var inputs = document.querySelectorAll("#surveyEntryForm input, #surveyEntryForm select");
+    for ( i = 0; i < inputs.length; i++ )
+    {
+        var input = inputs[i];
+        switch ( input.id )
+        {
+            case 'InputStoreNum': input.value=9832; continue;
+            case 'InputTransactionNum': input.value=1001; continue;
+            case 'InputDay': input.value=padLeft((new Date).getDate(), 2); continue;
+            case 'InputMonth': input.value=padLeft((new Date).getMonth()+1, 2); continue;
+            case 'InputYear': input.value=(new Date).getFullYear().toString().substring(2); continue;
+            case 'InputHour': input.value='01'; continue;
+            case 'InputMinute': input.value='01'; continue;
+            case 'InputMeridian': input.value='AM'; continue;
+            case 'InputTransactionNum': input.value=1001; continue;
+        }
+    }
+
+    // Next pages
+    var rows = document.querySelectorAll("#surveyForm TR, #surveyForm .inputtyperblv");
+    for ( i = 0; i < rows.length; i++ )
+    {
+        var inputs = rows[i].querySelectorAll('input[type=radio]');
+        if ( inputs.length )
+        {
+            // These are the 'Would you like to recognise someone' and 'Would you be interested in signing up' checkbox. Say no to avoid extra steps.
+            if ( inputs[0].name == 'R038000' || inputs[0].name == 'R048000' )
+                inputs[1].checked = true;
+            else
+                inputs[0].checked = true;
+        }
+    }
+
+    // Click Next
+    var nextButton = document.querySelector('#surveyEntryForm #NextButton, #surveyForm #NextButton');
+    if ( nextButton )
+        nextButton.click();
+
+
+    // Pad string nr to a length of n
+    function padLeft(nr, n, str){ return Array(n-String(nr).length+1).join(str||'0')+nr; }
+    // Get 12-based hour
+    function hours12(date) { return (date.getHours() + 24) % 12 || 12; }
+}());
---
+++ readme.md
@@ -0,0 +1,13 @@
+# KFC Feedback Autofill Chrome Extension
+
+Automatically completes the annoying KFC Feedback questionaire.
+
+![Demo](demo.gif)
+
+## Installation
+
+* `git clone` this repo
+* In Chrome navigate to *chrome://extensions/*
+* Check *Developer mode* checkbox
+* Click *Load unpacked extension* and navigate to this repos folder on your machine
+* Head to [http://kfcfeedback.com.au](http://kfcfeedback.com.au)
